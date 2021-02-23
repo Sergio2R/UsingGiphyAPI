49b93384479912aca80a15a03c3cdbfae1a1d47a
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2021-02-23 17:15:28 -0500

    implementar localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,12 +14,22 @@ export class GifsService {
   get historial() {
     return [...this._historial];
   }
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem("historial")!) || [];
+
+    // if(localStorage.getItem("historial")){
+    //   this._historial = JSON.parse(localStorage.getItem("historial")!);
+    // }
+  }
+
   buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=2HPEvcImQzXacVtKEKz2uBHh3rZ9LMh1&q=${query}&limit=10`)
       .subscribe((resp) => {
