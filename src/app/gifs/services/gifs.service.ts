74f6b3c9b344f08763889ce3a97d791cd16bece1
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2021-02-23 16:13:02 -0500

    peticion http con modulo HttpClientModule y observables

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  private apiKey : string = "2HPEvcImQzXacVtKEKz2uBHh3rZ9LMh1";
+  private apiKey: string = "2HPEvcImQzXacVtKEKz2uBHh3rZ9LMh1";
   private _historial: string[] = [];
   get historial() {
     return [...this._historial];
   }
-  buscarGifs(query: string) {
+  constructor(private http: HttpClient) { }
+  buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
-    // console.log(this._historial);
+    this.http.get("https://api.giphy.com/v1/gifs/search?api_key=2HPEvcImQzXacVtKEKz2uBHh3rZ9LMh1&q=hola&limit=10")
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      });
   }
 }
+
+
+//ejm async solo js
+  // async buscarGifs(query: string) {
+  //   query = query.trim().toLowerCase();
+  //   if (!this._historial.includes(query)) {
+  //     this._historial.unshift(query);
+  //     this._historial = this._historial.splice(0, 10);
+  //   }
+  //   //ejemplo con solo js
+  //   const resp = await fetch("https://api.giphy.com/v1/gifs/search?api_key=2HPEvcImQzXacVtKEKz2uBHh3rZ9LMh1&q=hola&limit=10")
+  //   const data = await resp.json();
+  //   console.log(data)
+  // }
+  //ejemplo con solo js
+    // fetch("https://api.giphy.com/v1/gifs/search?api_key=2HPEvcImQzXacVtKEKz2uBHh3rZ9LMh1&q=hola&limit=10")
+    //   .then(resp => {
+    //     resp.json().then(data => {
+    //       console.log(data)
+    //     }
+    //     )
+    //   })
