3d00ffa801bfe2d0f33ef8dfc2188f6bca38930f
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2021-02-23 13:47:25 -0500

    @ViewChild obtener refs de html

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BusquedaComponent{
+  //(!) not null assertion
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  buscar (){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = "";
   }
-
 }
