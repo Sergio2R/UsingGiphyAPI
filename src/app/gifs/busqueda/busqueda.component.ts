5c3da967c1d4df8f07d9992a4112bc40f022eed8
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2021-02-23 14:49:37 -0500

    Service Gifs y consumo

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,9 +10,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent{
   //(!) not null assertion
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService){}
+
   buscar (){
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = "";
   }
 }
